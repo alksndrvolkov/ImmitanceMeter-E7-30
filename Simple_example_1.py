42d2fc22892de28a7974c49a993ccf921c2cf3d4
alksndrvolkov <109286539+alksndrvolkov@users.noreply.github.com>
2026-03-05 15:28:31 +0300

    Update Simple_example_1.py

--- Simple_example_1.py
+++ Simple_example_1.py
@@ -4,10 +4,10 @@ from E7_30_ImmittanceMeter import ImmittanceMeter
 meter = ImmittanceMeter("COM2", 0.2)
 meter.set_frequency(1100) # частота в Гц
 meter.set_bias_voltage(0.15) # напряжение смещения в В
-time.sleep(1) # если скорость измерений на приборе стоит "средне", ждём первое измерение
 impedance_module, impedance_phase = meter.read_impedance()
 print("Impedance module: ", impedance_module, " Ω")
 print("Impedance phase: ", impedance_phase, " °")
 
 meter.close_serial()
+
 
